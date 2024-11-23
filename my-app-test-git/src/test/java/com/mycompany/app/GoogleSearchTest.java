8ce84bbaad186bb9a1fc32fa49c5635dfb8d971e
Markoradonic <markoradonic90@gmail.com>
2024-11-23 19:59:11 +0100

    Initial commit

---
+++ my-app-test-git/src/test/java/com/mycompany/app/GoogleSearchTest.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+
+	@Test
+	public void helloWorld() {
+		System.out.println("Hello World!");
+	}
+	
+	@Test
+	public void helloWorld2() {
+		System.out.println("Hello World!");
+	}
+}
